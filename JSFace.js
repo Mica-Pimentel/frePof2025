bb665498ff2d2f6289a234c1da8537996f6c8716
Micael Pimentel <p.micael.mp@gmail.com>
2025-10-13 21:58:35 -0300

    detecáo face

---
+++ JSFace.js
@@ -0,0 +1,140 @@
+import {
+  FaceDetector,
+  FilesetResolver,
+  Detection
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+
+const demosSection = document.getElementById("demos");
+
+let faceDetector;
+let runningMode = "VIDEO";
+
+const initializefaceDetector = async () => {
+  const vision = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+  );
+  faceDetector = await FaceDetector.createFromOptions(vision, {
+    baseOptions: {
+      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.tflite`,
+      delegate: "GPU"
+    },
+    runningMode: runningMode
+  });
+  demosSection.classList.remove("invisible");
+};
+initializefaceDetector();
+
+let video = document.getElementById("webcam");
+const liveView = document.getElementById("liveView");
+let enableWebcamButton;
+
+// Check if webcam access is supported.
+const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
+
+// Keep a reference of all the child elements we create
+var children = [];
+
+if (hasGetUserMedia()) {
+  enableWebcamButton = document.getElementById("webcamButton");
+  enableWebcamButton.addEventListener("click", enableCam);
+} else {
+  console.warn("getUserMedia() is not supported by your browser");
+}
+
+// Enable the live webcam view and start detection.
+async function enableCam(event) {
+  if (!faceDetector) {
+    alert("Face Detector is still loading. Please try again..");
+    return;
+  }
+
+  enableWebcamButton.classList.add("removed");
+
+  const constraints = {
+    video: true
+  };
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (stream) {
+      video.srcObject = stream;
+      video.addEventListener("loadeddata", predictWebcam);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+let lastVideoTime = -1;
+async function predictWebcam() {
+  let startTimeMs = performance.now();
+
+  if (video.currentTime !== lastVideoTime) {
+    lastVideoTime = video.currentTime;
+    const detections = faceDetector.detectForVideo(video, startTimeMs).detections;
+    displayVideoDetections(detections);
+  }
+
+  window.requestAnimationFrame(predictWebcam);
+}
+
+function displayVideoDetections(detections) {
+  for (let child of children) {
+    liveView.removeChild(child);
+  }
+  children.splice(0);
+
+  for (let detection of detections) {
+    const p = document.createElement("p");
+    p.innerText =
+      "Confidence: " +
+      Math.round(parseFloat(detection.categories[0].score) * 100) +
+      "% .";
+    p.style =
+      "left: " +
+      (video.offsetWidth -
+        detection.boundingBox.width -
+        detection.boundingBox.originX) +
+      "px;" +
+      "top: " +
+      (detection.boundingBox.originY - 30) +
+      "px; " +
+      "width: " +
+      (detection.boundingBox.width - 10) +
+      "px;";
+
+    const highlighter = document.createElement("div");
+    highlighter.setAttribute("class", "highlighter");
+    highlighter.style =
+      "left: " +
+      (video.offsetWidth -
+        detection.boundingBox.width -
+        detection.boundingBox.originX) +
+      "px;" +
+      "top: " +
+      detection.boundingBox.originY +
+      "px;" +
+      "width: " +
+      (detection.boundingBox.width - 10) +
+      "px;" +
+      "height: " +
+      detection.boundingBox.height +
+      "px;";
+
+    liveView.appendChild(highlighter);
+    liveView.appendChild(p);
+
+    children.push(highlighter);
+    children.push(p);
+    for (let keypoint of detection.keypoints) {
+      const keypointEl = document.createElement("spam");
+      keypointEl.className = "key-point";
+      keypointEl.style.top = `${keypoint.y * video.offsetHeight - 3}px`;
+      keypointEl.style.left = `${
+        video.offsetWidth - keypoint.x * video.offsetWidth - 3
+      }px`;
+      liveView.appendChild(keypointEl);
+      children.push(keypointEl);
+    }
+  }
+}
